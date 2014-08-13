c2a4dd3cd3a9d8541b992ce94464d1b14c2bd8d3
Yuri D'Elia <yuri.delia@eurac.edu>
2014-08-13 15:06:07 +0200

    Move drivers to the top level.

--- bond/JavaScript/__init__.py
+++
@@ -1,7 +0,0 @@
-from bond import *
-import os
-
-def JavaScript(cmd=None, args=None, xargs='', cwd=None, env=os.environ,
-               trans_except=False, protocol=None, compat=None, timeout=None, logfile=None):
-    return bond('JavaScript', cmd, args, xargs, cwd=cwd, env=env,
-                trans_except=trans_except, timeout=timeout, logfile=logfile)
--- bond/Python/__init__.py
+++
@@ -1,7 +0,0 @@
-from bond import *
-import os
-
-def Python(cmd=None, args=None, xargs='', cwd=None, env=os.environ,
-           trans_except=True, protocol=None, compat=None, timeout=None, logfile=None):
-    return bond('Python', cmd, args, xargs, cwd=cwd, env=env,
-                trans_except=trans_except, timeout=timeout, logfile=logfile)
